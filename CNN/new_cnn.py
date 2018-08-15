e8289181355d09afcfbcf6c00360ca07c18638fc
Hannah Beilinson <hcbeilinson@gmail.com>
2018-08-15 13:24:00 -0700

    Changed new_cnn and the results seem to be improving

---
+++ CNN/new_cnn.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+#Get rid of those annoying numpy/tensorflow warnings
+import warnings
+warnings.filterwarnings("ignore", message="numpy.dtype size changed")
+warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
+
+import keras
+import numpy as np
+from keras.layers import *
+from keras.models import Sequential
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from sklearn.model_selection import train_test_split
+import matplotlib.pylab as plt
+from liveHistCallback import *
+
+batch_size = 20
+epochs = 25
+
+
+in_data = np.load("indata.npy")
+img_x, img_y = in_data[0].shape
+in_data = in_data.reshape(in_data.shape[0], img_x, img_y, 1)
+out_data = np.load("outdata.npy")
+
+seed=7
+np.random.seed(seed)
+x_train, x_test, y_train, y_test = train_test_split(in_data, out_data, random_state=seed)
+
+# model = unet(img_x, img_y, 1)
+def cnn(img_x, img_y):
+    model = Sequential()
+    model.add(MaxPooling2D(pool_size=(4, 4)))
+    model.add(Conv2D(64, kernel_size=(4, 4),
+                        activation='relu'
+                     # input_shape = (img_x, img_y, 1)
+                     ))
+    model.add(Dropout(0.1))
+    # model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Conv2D(64, kernel_size=(2, 2), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    # model.add(Dense(1000, activation='relu'))
+    model.add(Dense(500, kernel_initializer='normal', activation='relu'))
+    # model.add(Dense(50, kernel_initializer='normal', activation='relu'))
+    # model.add(Dense(5, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(1, kernel_initializer='normal'))
+    #
+    # model.summary()
+    return model
+
+model = cnn(img_x, img_y)
+model.compile(loss=keras.losses.mean_squared_error,
+                # optimizer=keras.optimizers.Adam(lr = 1e-5),
+                optimizer=keras.optimizers.SGD(nesterov=True),
+                metrics=['mse', 'mae'])
+
+history = liveHist()
+
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test),
+          callbacks=[history])
+
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+plt.savefig("lossaccplot.png", dpi='figure')
+
+model_json = model.to_json()
+with open("convnet1.json", "w") as json_file:
+    json_file.write(model_json)
+model.save_weights("convnet1.h5")
---
+++ CNN/predict_on_map.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+#Get rid of those annoying numpy/tensorflow warnings
+import warnings
+warnings.filterwarnings("ignore", message="numpy.dtype size changed")
+warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
+
+import keras
+from PIL import Image
+from keras.models import model_from_json
+import numpy as np
+from os import getcwd
+from tqdm import trange, tqdm
+import matplotlib.pylab as plt
+
+# in_data = np.load("indata_for_prediction.npy")
+# img_x, img_y = in_data[0].shape
+# in_data = in_data.reshape(in_data.shape[0], img_x, img_y, 1)
+#
+# input_points = np.load("input_prediction_points.npy")
+
+def get_input_data(p, i, j, kernel_size, h, w):
+    min_i = int(i-(kernel_size-1)/2)
+    max_i = int(i+(kernel_size-1)/2)
+    min_j = int(j-(kernel_size-1)/2)
+    max_j = int(j+(kernel_size-1)/2)
+
+    data = []
+    for k in range(min_i, max_i):
+        row = [1 if k<0 or l<0 or k>=h or l>=w or p[k][l] <254 else 0 for l in range(min_j, max_j)]
+        data.append(row)
+        # for l in range(min_j, max_j):
+        #     # if k != i and l != j:
+        #     if k<0 or l<0 or k>=h or l>=w:
+        #         data[k-min_i].append(1)
+        #     elif p[k][l] >= 254:
+        #         data[k-min_i].append(0)
+        #     else:
+        #         data[k-min_i].append(1)
+
+    return np.array(data)
+
+# load json and create model
+json_file = open('convnet1.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("convnet1.h5")
+
+loaded_model.summary()
+
+path = 'input_maps/maze.pgm'
+# data_path = path + 'etu_1_condensed/'
+
+I = Image.open(path)
+p = np.asarray(I).astype('int')
+w, h = I.size
+im = np.zeros((w, h), dtype=int)
+im_log = np.zeros((w, h), dtype=int)
+
+kernel_size = 61
+
+for i in trange(h, position=1):
+    for j in range(w):
+        if p[i][j] >= 254:
+            in_data = get_input_data(p, i, j, kernel_size, h, w)
+            img_x, img_y = in_data.shape
+            in_data = in_data.reshape(1, img_x, img_y, 1)
+            val = loaded_model.predict(in_data, verbose=0)
+
+            # if len(val) != 1:
+            # im[i][j] = val
+            if val[0][0] >= 0:
+            # tqdm.write("log val: " + str(val[0][0]))
+                im_log[i][j] = np.log(val[0][0])-1
+
+plt.imshow(im_log, cmap=plt.cm.Greys, interpolation='nearest')
+plt.show()
+
+# np.save("predicted_sim_data", im)
