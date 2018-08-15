2766047cb80db35e7c7f95603eac5de32bf41693
Hannah Beilinson <hcbeilinson@gmail.com>
2018-08-14 17:07:38 -0700

    Made some good changes to make_cnn_dataset, about to copy over Sage's new code

--- CNN/cnn.py
+++ CNN/cnn.py
@@ -9,18 +9,17 @@ warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
 
 import keras
 import numpy as np
-from keras.datasets import mnist
-from keras.layers import Dense, Flatten
-from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import *
+# from keras.layers import Conv2D, MaxPooling2D
 from keras.models import Sequential
 from keras.callbacks import History
 from sklearn.model_selection import train_test_split
 import matplotlib.pylab as plt
 
-batch_size = 128
-epochs = 300
+batch_size = 200
+epochs = 50
 
-img_x, img_y = 30, 30
+img_x, img_y = 60, 60
 
 in_data = np.load("indata.npy")
 in_data = in_data.reshape(in_data.shape[0], img_x, img_y, 1)
@@ -30,23 +29,31 @@ seed=7
 np.random.seed(seed)
 x_train, x_test, y_train, y_test = train_test_split(in_data, out_data, test_size=.15, random_state=seed)
 
-model = Sequential()
-model.add(Conv2D(32, kernel_size=(5, 5), strides=(1, 1),
-                 activation='relu',
-                 input_shape=(img_x, img_y, 1)))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-model.add(Conv2D(64, (5, 5), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Flatten())
-model.add(Dense(1000, activation='relu'))
-model.add(Dense(500, kernel_initializer='normal', activation='relu'))
-model.add(Dense(50, kernel_initializer='normal', activation='relu'))
-model.add(Dense(5, kernel_initializer='normal', activation='relu'))
-model.add(Dense(1, kernel_initializer='normal'))
+def cnn(img_x, img_y):
+    model = Sequential()
+    model.add(MaxPooling2D(pool_size=(4, 4), strides=(4, 4)))
+    model.add(Conv2D(64, kernel_size=(4, 4), strides=(1, 1),
+                     activation='relu',
+                     input_shape=(img_x, img_y, 1)))
+    model.add(Dropout(0.1))
+    # model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Conv2D(64, (2, 2), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    # model.add(Dense(1000, activation='relu'))
+    model.add(Dense(500, kernel_initializer='normal', activation='relu'))
+    # model.add(Dense(50, kernel_initializer='normal', activation='relu'))
+    # model.add(Dense(5, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(1, kernel_initializer='normal'))
+    #
+    # model.summary()
+    return model
 
+model = cnn(img_x, img_y)
 model.compile(loss=keras.losses.mean_squared_logarithmic_error,
-              optimizer=keras.optimizers.Adam(),
-              metrics=['mse', 'mae'])
+                # optimizer=keras.optimizers.Adam(lr = 1e-5),
+                optimizer='adam',
+                metrics=['mse', 'mae'])
 
 class liveHist(keras.callbacks.Callback):
     def on_train_begin(self, logs={}):
@@ -81,13 +88,13 @@ class liveHist(keras.callbacks.Callback):
         self.ax3.set_title('Validation Accuracy (MAE)')
         self.ax3.set(ylabel='Mean Absolute Error')
 
-        self.ax1.plot(self.x, self.losses, label="loss", color='C0')
-        self.ax1.plot(self.x, self.val_losses, label="val_loss", color='C1')
+        self.ax1.plot(self.x, self.losses, label="loss")
+        self.ax1.plot(self.x, self.val_losses, label="val_loss")
         self.ax1.legend(loc="upper right")
         self.ax2.plot(self.x, self.mse_acc)
         self.ax3.plot(self.x, self.mae_acc)
 
-        # plt.pause(.01)
+        plt.pause(.01)
         plt.draw()
 
 
--- CNN/make_cnn_dataset.py
+++ CNN/make_cnn_dataset.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-
 #Thanks to sagekg
-#Debug this!!
 
 import numpy as np
 from PIL import Image
@@ -10,34 +8,30 @@ import argparse, glob, csv
 from os import getcwd
 
 def get_input_data(p, i, j, kernel_size, h, w):
-    min_i = int(i-(kernel_size-1)/2)
-    max_i = int(i+(kernel_size-1)/2)
-    min_j = int(i-(kernel_size-1)/2)
-    max_j = int(i+(kernel_size-1)/2)
+    min_i = int(i-(kernel_size)/2)
+    max_i = int(i+(kernel_size)/2)
+    min_j = int(j-(kernel_size)/2)
+    max_j = int(j+(kernel_size)/2)
 
     data = []
     for k in range(min_i, max_i):
-        data.append([])
-        for l in range(min_j, max_j):
-            # if k != i and l != j:
-            if k<0 or l<0 or k>=h or l>=w:
-                data[k-min_i].append(1)
-            elif p[k][l] >= 254:
-                data[k-min_i].append(0
-                )
-            else:
-                data[k-min_i].append(1)
+        row = [1 if k<0 or l<0 or k>=h or l>=w or p[k][l] <254 else 0 for l in range(min_j, max_j)]
+        data.append(row)
+        # data.append([])
+        # for l in range(min_j, max_j):
+        #     # if k != i and l != j:
+        #     if k<0 or l<0 or k>=h or l>=w:
+        #         data[k-min_i].append(1)
+        #     elif p[k][l] >= 254:
+        #         data[k-min_i].append(0
+        #         )
+        #     else:
+        #         data[k-min_i].append(1)
 
     return np.array(data)
 
 if __name__ == "__main__":
     path = getcwd() + '/'
-
-    # open('indata.csv', 'w')
-    # indata = csv.writer(open('indata.csv', 'a'))
-    #
-    # open('outdata.csv', 'w')
-    # outdata = csv.writer(open('outdata.csv', 'a'))
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', action='store',default=path+'input_maps/',
@@ -50,7 +44,7 @@ if __name__ == "__main__":
     outpath = results.outpath
 
     #Size of one side of the scanning window
-    kernel_size = 31
+    kernel_size = 60
 
     #get ordered lists of files
     infiles = sorted(glob.glob(inpath + '*'))
@@ -66,18 +60,15 @@ if __name__ == "__main__":
         output_im = Image.open(outfiles[i])
         output_data = np.asarray(output_im)
 
-        input_data_len = kernel_size*kernel_size
-
         input_data = []
         output_list = []
 
         for i in trange(h, position=1):
             for j in range(w):
-                if p[i][j] >= 254 and output_data[i][j] != 0:
-                    input_data.append(get_input_data(p, i, j, kernel_size, h, w))
-                    output_list.append(output_data[i][j])
-                    # indata.writerow(get_input_data(p, i, j, kernel_size, h, w))
-                    # outdata.writerow([output_data[i][j]])
+                # if p[i][j] >= 254 and output_data[i][j] != 0:
+                input_data.append(get_input_data(p, i, j, kernel_size, h, w))
+                output_list.append(output_data[i][j])
+
 
         np.save("indata",np.array(input_data))
         np.save("outdata", np.array(output_list))
